5c48c009f7f838d4f29be1a755d3889e03ac2833
ViniciusMendes19 <71828304+ViniciusMendes19@users.noreply.github.com>
2022-02-28 18:35:56 -0300

    Add files via upload

---
+++ SpaceNews-master/js/main.js
@@ -0,0 +1,13 @@
+$('.section-primary-slider').slick({
+infinite: true,
+speed: 300,
+autoplay: true,
+autoplaySpeed: 3500,
+});
+
+$('.banner-slider').slick({
+infinite: true,
+speed: 300,
+autoplay: true,
+autoplaySpeed: 3500,
+});
